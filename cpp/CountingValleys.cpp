46b54b57fba4e86837c3c3935185c29d09f47bdd
Taehyun Cho <tcho5@binghamton.edu>
2018-09-11 21:38:06 -0400

    Create CountingValleys.cpp

---
+++ cpp/CountingValleys.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the countingValleys function below.
+int countingValleys(int n, string s) {
+    int numVals = 0;
+    int levels = 0;
+    for(int i = 0; i < n; i++){
+        cout << s[i] << " || " << "U" << endl;
+        if(s[i] == 'U'){
+            levels++;
+            if(levels == 0) numVals++;
+        }
+        else levels--;
+        
+    }
+    return numVals;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int result = countingValleys(n, s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
