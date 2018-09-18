027d687af7c3052a2b7755f01e0c9187dfa99e98
Taehyun Cho <tcho5@binghamton.edu>
2018-09-17 23:10:00 -0400

    Update 2DArrays.py

--- Python/Arrays/2DArrays.py
+++ Python/Arrays/2DArrays.py
@@ -1,1 +1,34 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    maxVal = -999
+    counter = 1
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if (i+2) < len(arr) and (j+2) < len(arr):
+                total = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2] 
+                if maxVal <= total:
+                    maxVal = total
+                    print("index starting at: " + str(i))
+    return maxVal 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
